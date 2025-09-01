8cb6ca6adf5c1fe5b50533a8e7e115a3dfe47c8d
Emma-code1 <emmanuelcastilloleyva@gmail.com>
2025-09-01 18:52:03 +0000

    Programa nombre

---
+++ nombre.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+int impresion;
+char nombre[15];
+
+printf("Bienvenido, escribe tu nombre\n");
+scanf("&s", nombre);
+printf("Cuantas veces deseas imprimirlo?\n");
+scanf("&i", &impresion);
+
+for(int i = 1; i <= impresion; i++){
+printf("&s", nombre);
+}
+}
